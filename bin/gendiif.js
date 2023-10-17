b64388e88e1f56ae9271fbcb14672d671ce0f17a
JohnyTest12345 <maksahater@gmail.com>
2023-10-17 13:25:02 +0500

    add two lesson commander.js

--- bin/gendiif.js
+++ bin/gendiif.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .argument('<filepath1>', 'path to these file 1')
+  .argument('<filepath2>', 'path to these file 2')
+  .option('-f, --format <type>', 'output format')
 
   program.parse();
